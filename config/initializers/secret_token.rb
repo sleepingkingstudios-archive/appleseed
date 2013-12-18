ef0f689a9bc8d190120edbe5aa545757c8369841
sleepingkingstudios <sleeping.king.studios@gmail.com>
2013-12-18 14:28:36 -0500

    Initial commit.
    " Come, my friends. 'tis not too late to seek a newer world.
      Push off, and sitting well in order smite the sounding furrows,
      for my purpose holds to sail beyond the sunset,
      and the baths of all the western stars, until I die.
      It may be the gulfs will wash us down;
      it may be we shall touch the Happy Isles,
      and see the great Achilles, whom we knew."
    
    -- Ulysses, Alfred, Lord Tennyson

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Appleseed::Application.config.secret_key_base = '6be384fc18badefd9ffbaccd3b07ad5a66fb8093546a237c855b4b8f0aece8016b292273f98de65478b17c61d535c6cf82a94b0860b9762b0adac17e14a06d7f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Appleseed::Application.config.session_store :cookie_store, key: '_appleseed_session'

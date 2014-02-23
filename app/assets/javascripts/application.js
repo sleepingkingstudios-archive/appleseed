80dbeba5250b6fd827f51427ac73e5d8b0ab02c0
sleepingkingstudios <sleeping.king.studios@gmail.com>
2014-02-23 14:48:44 -0500

    Add Underscore, Backbone+Marionette dependencies.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,16 @@
+// Require core libraries.
 //= require jquery
 //= require jquery_ujs
 //= require jquery.autosize.min
+//= require underscore
+
+// Require responsive framework files.
 //= require foundation
+
+// Require JavaScript framework files.
+//= require backbone
+//= require backbone.marionette
+
 //= require_tree .
 
 $(function(){ $(document).foundation(); });

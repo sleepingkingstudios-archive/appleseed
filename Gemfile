84e0e8c45908042957b2890d6643b32589414874
sleepingkingstudios <sleeping.king.studios@gmail.com>
2014-06-23 00:40:19 -0400

    Update to Rails 4.1, RSpec 3.
    Current spec status: 313 examples, 0 failures in 3.95 seconds.

--- Gemfile
+++ Gemfile
@@ -1,21 +1,21 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.0.2'
+gem 'rails', '4.1.1'
 
 ### App Server ###
-# gem 'unicorn'
+gem 'unicorn'
 
 ### Datastore ###
-gem 'mongoid',  '4.0.0.beta1', github: 'mongoid/mongoid'
+gem 'mongoid',  '4.0.0.rc2'
 gem 'devise',   '~> 3.2.2'
 
-gem 'mongoid-sleeping_king_studios', '~> 0.7', '>= 0.7.8'
+gem 'mongoid-sleeping_king_studios', '~> 0.7', '>= 0.7.9'
 
 ### Assets ###
 gem 'haml-rails',        '~> 0.4'
-gem 'sass-rails',        '~> 4.0.0'
-gem 'coffee-rails',      '~> 4.0.0'
-gem 'jquery-rails',      '~> 3.0.4'
+gem 'sass-rails',        '~> 4.0', '>= 4.0.3'
+gem 'coffee-rails',      '~> 4.0'
+gem 'jquery-rails',      '~> 3.1'
 gem 'uglifier',          '>= 1.3.0' # Compressor for JavaScript assets
 gem 'compass-rails',     '~> 1.1.2'
 gem 'foundation-rails',  '~> 5.0.3'
@@ -27,6 +27,9 @@ gem 'redcarpet', '~> 3.0.0'
 # gem 'debugger', group: [:development, :test]
 gem 'jbuilder', '~> 1.2' # Build JSON APIs. Read more: https://github.com/rails/jbuilder
 # gem 'turbolinks' # Read more: https://github.com/rails/turbolinks
+
+### App Preloader ###
+gem 'spring', :group => :development
 
 ### Documentation ###
 group :doc do
@@ -42,9 +45,8 @@ end # group
 
 ### Testing ###
 group :test do
-  gem 'rspec-rails',                 '~> 2.14.0'
-  gem 'rspec-sleeping_king_studios', '>= 1.0.1'
-  gem 'fuubar',                      '~> 1.2.1'
+  gem 'rspec-rails',                 '~> 3.0'
+  gem 'rspec-sleeping_king_studios', '2.0.0.alpha'
   gem 'factory_girl_rails',          '~> 4.2.0'
   gem 'database_cleaner',            '~> 1.2.0'
 end # group
@@ -55,4 +57,4 @@ group :production do
   gem 'rails_12factor', '~> 0.0.2'
 end # group
 
-ruby "2.1.0"
+ruby "2.1.2"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,26 +3,53 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+  # Automatically mix in different behaviours to your tests based on their
+  # file location.
+  config.infer_spec_type_from_file_location!
+
+  # Limit a spec run to individual examples or groups by tagging them with
+  # `:focus` metadata.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  Kernel.srand config.seed
 
   # Alias "it should behave like" to 2.13-like syntax.
   config.alias_it_should_behave_like_to 'expect_behavior', 'has behavior'
+
+  # Allow more verbose output when running an individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured.
+    config.default_formatter = 'doc'
+  end # if
+
+  # rspec-expectations config goes here.
+  config.expect_with :rspec do |expectations|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    expectations.syntax = :expect
+  end # expect_with
+
+  # rspec-mocks config goes here.
+  config.mock_with :rspec do |mocks|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    mocks.syntax = :expect
+
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended.
+    mocks.verify_partial_doubles = true
+  end # mock_with
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
